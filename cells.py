67ba6626bb4f6b595a3d517a1134a48d77df8c40
SynedraAcus <alexeymorozov1991@gmail.com>
2017-02-22 22:00:08 +0800

    Cell and CellField classes

--- cells.py
+++ cells.py
@@ -1,4 +1,17 @@
-class Cell():
+"""
+Classes for various stuff that can be placed on a map
+"""
+
+
+#  This class should go to something like util.py, when I make that file
+class StackCityException(Exception):
+    """
+    An exception raised within StackCity engine
+    """
+    pass
+
+
+class Cell:
     """
     A cell backend class.
     This class is an ObjectProperty of FieldCell and contains all the game-relevant
@@ -8,16 +21,42 @@ class Cell():
     It also stores a cell number which is set when the cell is placed into a
     CellGrid and can later be used to look up its neighbours.
     """
-    def __init__(self, ground='empty', bonus=None, built=False):
-        self.ground = ground
+    def __init__(self, ground=None, bonus=None):
+        if ground:
+            self.ground = ground
+        else:
+            self.ground = Ground('empty')
         self.bonus = bonus
         self.built = False
         self.number = None
         # A ref to the building, should it be placed on this cell
         self.building = None
 
+    def can_accept(self, item):
+        """
+        Return True if this item can be added to this cell
+        :param item:
+        :return:
+        """
+        # Currently a placeholder
+        if isinstance(item, Ground):
+            return True
+        else:
+            return False
 
-class CellField():
+    def add_item(self, item):
+        """
+        Attach an item to this cell
+        :param item:
+        :return:
+        """
+        if isinstance(item, Ground):
+            self.ground = item
+        else:
+            raise StackCityException('Incorrect item type added to cell')
+
+
+class CellField:
     """
     A grid of cells.
     This class is a list of cells. It supports operations such as addressing any
@@ -39,8 +78,19 @@ class CellField():
             raise StackCityException('Adding cell to a field that is full!')
 
 
-class StackCityException(Exception):
+class Ground:
     """
-    An exception raised within StackCity engine
+    A class for ground type
     """
-    pass
+    ground_types = {'empty', 'water', 'living', 'military', 'infrastructure'}
+
+    def __init__(self, ground_type):
+        if not ground_type in self.ground_types:
+            raise StackCityException('Unknown ground type')
+        self.ground_type = ground_type
+
+    # String representation just in case
+    def __str__(self):
+        return self.ground_type
+
+
